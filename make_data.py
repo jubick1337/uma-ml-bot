cb33f10ed5dca0e47529eb0d83cbbbabb0d213af
jubick1337 <mattyson.so@gmail.com>
2019-11-08 17:21:06 +0500

    added data tools

---
+++ make_data.py
@@ -0,0 +1,42 @@
+import pickle
+import pandas as pd
+import pprint
+import numpy as np
+from PIL import Image
+import os
+from shutil import copyfile
+
+data = pd.read_csv('images_labelling.csv')
+
+print('found ' + str(len(pd.unique(data['label']))) + ' unique labels')
+
+print('found ' + str(len(pd.unique(data['class_']))) + ' unique classes')
+
+mask = np.random.rand(len(data)) < 0.8
+
+train = data[mask]
+val = data[~mask]
+
+
+if not os.path.exists('images_train'):
+    os.mkdir('images_train')
+    print('Making training set')
+    for index, row in train.iterrows():
+        src = os.getcwd() + "\\images\\" + str(row['boxid']) + ".png"
+        if not os.path.exists('images_train\\' + str(row['label'])):
+            os.mkdir('images_train\\' + str(row['label']))
+        dst = os.getcwd() + "\\images_train\\" + str(row['label']) + "\\" + str(row['boxid']) + ".png"
+        copyfile(src, dst)
+
+if not os.path.exists('images_val'):
+    os.mkdir('images_val')
+    print('Making val set')
+    for index, row in val.iterrows():
+        src = os.getcwd() + "\\images\\" + str(row['boxid']) + ".png"
+        if not os.path.exists('images_val\\' + str(row['label'])):
+            os.mkdir('images_val\\' + str(row['label']))
+        dst = os.getcwd() + "\\images_val\\" + str(row['label']) + "\\" + str(row['boxid']) + ".png"
+        copyfile(src, dst)
+
+
+print('Done')
