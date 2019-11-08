bb8da2631f92b5c0194940d8bf6d61fb63c3ddde
jubick1337 <mattyson.so@gmail.com>
2019-11-08 16:54:58 +0500

    initial commit

---
+++ bot.py
@@ -0,0 +1,51 @@
+import os
+import telebot
+from predict import Prediction
+from flask import Flask, request
+
+TOKEN = '1028491931:AAHHw3d43K2NWVIYLOWlQoLYf9bPmr4IDLc'
+bot = telebot.TeleBot(TOKEN)
+server = Flask(__name__)
+model = Prediction()
+
+
+@bot.message_handler(content_types=['photo'])
+def predict(message):
+    file_id = message.photo[-1].file_id
+    file_info = bot.get_file(file_id)
+    downloaded_file = bot.download_file(file_info.file_path)
+
+    with open('source_img.png', 'wb') as new_file:
+        new_file.write(downloaded_file)
+
+    res = model.predict('source_img.png')
+    os.remove('source_img.png')
+
+    bot.reply_to(message, str(res))
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, 'Hello, ' + message.from_user.first_name + " !")
+
+
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def echo_message(message):
+    bot.reply_to(message, message.text)
+
+
+@server.route('/' + TOKEN, methods=['POST'])
+def get_message():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
+
+
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url='https://uma-ml-bot.herokuapp.com/' + TOKEN)
+    return "!", 200
+
+
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
---
+++ predict.py
@@ -0,0 +1,58 @@
+from tensorflow.python.keras.backend import set_session
+import keras.backend as K
+import tensorflow as tf
+from keras.preprocessing.image import load_img
+from keras.preprocessing.image import img_to_array
+from keras.layers.normalization import BatchNormalization
+from keras.layers.convolutional import Conv2D
+from keras.layers.pooling import MaxPooling2D
+from keras.layers import GlobalAveragePooling2D, Dense, Dropout, Flatten, Input
+from keras.models import Sequential
+import numpy as np
+
+
+class Prediction:
+    def __create_model(self):
+        model = Sequential()
+
+        model.add(Conv2D(32, (3, 3), activation='relu', padding='same',
+                         input_shape=(224, 224, 3)))
+        model.add(MaxPooling2D((2, 2), padding='same'))
+
+        model.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
+        model.add(MaxPooling2D((2, 2), padding='same'))
+
+        model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
+        model.add(MaxPooling2D((2, 2), padding='same'))
+
+        model.add(Conv2D(256, (3, 3), activation='relu', padding='same'))
+        model.add(MaxPooling2D((2, 2), padding='same'))
+
+        model.add(GlobalAveragePooling2D())
+
+        model.add(Dense(256, activation='relu'))
+
+        model.add(BatchNormalization())
+
+        model.add(Dense(25, activation='softmax'))
+        return model
+
+    def predict(self, path):
+        K.clear_session()
+        img = img_to_array(load_img(path, target_size=(224, 224))) / 255
+        print(type(img))
+        print(img.shape)
+        img = img.reshape([-1, 224, 224, 3])
+        global sess
+        global graph
+        with self.graph.as_default():
+            set_session(self.sess)
+            p = self.model.predict(img)
+            return np.argmax(p)
+
+    def __init__(self):
+        self.sess = tf.Session()
+        self.graph = tf.get_default_graph()
+        set_session(self.sess)
+        self.model = self.__create_model()
+        self.model.load_weights('model.h5')
